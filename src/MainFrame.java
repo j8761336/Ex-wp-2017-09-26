7e87ecbdcaede231573c251b240c1445e25b85d2
j8761336 <105021026@live.asia.edu.tw>
2017-09-26 12:39:09 +0800

    yo

---
+++ src/MainFrame.java
@@ -0,0 +1,62 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class MainFrame extends Frame{
+    private Timer T1;
+    public MainFrame(){
+        initComp();
+    }
+    private Button btn =new Button("Add Title!");
+    private Button btn2=new Button("Start Right!");
+    private Label lab =new Label("Look me YOOOO!!");
+    private int count = 0;
+    private int count2 = 50;
+    public void initComp() {
+        this.setBounds(250, 100, 500, 500);
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        this.setLayout(null);
+        lab.setBounds(50,50,120,30);
+        this.add(lab);
+        btn.setBounds(150,150,150,80);
+        this.add(btn);
+
+        btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                count++;
+                MainFrame.this.setTitle(Integer.toString(count++));
+            }
+        });
+
+        btn2.setBounds(150,300,150,80);
+        this.add(btn2);
+
+        btn2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                count2++;
+                lab.setBounds(count2,50,120,30);
+                T1.start();
+            }
+        });
+
+        T1=new Timer(50,new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                count2++;
+                lab.setBounds(count2,50,120,30);
+            }
+        });
+
+    }
+
+
+}
