06a12059bfba5cc9f32bfd503db8033e631aae4c
j8761336 <105021026@live.asia.edu.tw>
2017-11-07 11:22:52 +0800

    123
    123

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -11,8 +11,11 @@ public class MainFrame extends Frame{
     public MainFrame(){
         initComp();
     }
-    private Button btn =new Button("Add Title!");
-    private Button btn2=new Button("Start Right!");
+    private Button btn =new Button("Add !");
+    private Button btn2=new Button("Start Moving!");
+    private Button btn3=new Button("Sub!");
+    private Button btn4=new Button("Exit!");
+
     private Label lab =new Label("Look me YOOOO!!");
     private int count = 0;
     private int count2 = 50;
@@ -27,19 +30,45 @@ public class MainFrame extends Frame{
         this.setLayout(null);
         lab.setBounds(50,50,120,30);
         this.add(lab);
-        btn.setBounds(150,150,150,80);
+        btn.setBounds(50,150,80,30);
         this.add(btn);
 
+        btn2.setBounds(50,300,80,30);
+        this.add(btn2);
+
+        btn3.setBounds(300,150,80,30);
+        this.add(btn3);
+
+        btn4.setBounds(300,300,80,30);
+        this.add(btn4);
         btn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                count++;
+//                count++;
+                count2++;
                 MainFrame.this.setTitle(Integer.toString(count++));
+                lab.setBounds(count2,50,120,30);
             }
         });
 
-        btn2.setBounds(150,300,150,80);
-        this.add(btn2);
+        btn3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+//                count--;
+                count2--;
+                MainFrame.this.setTitle(Integer.toString(count--));
+                lab.setBounds(count2,50,120,30);
+            }
+        });
+
+        btn4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+
 
         btn2.addActionListener(new ActionListener() {
             @Override
